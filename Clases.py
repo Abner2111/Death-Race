9f4aa303fefe3aa96a51d69a20c26d2918b97ea6
Abner2111 <[abnerAQ73@gmail.com]>
2019-05-09 10:04:40 -0600

    funciones de movimiento de enemigo

--- Clases.py
+++ Clases.py
@@ -1,7 +1,8 @@
 import pygame
 import csv
 import json
-
+import random
+import math
 
 class General(pygame.sprite.Sprite): #clase general
     allsprites = pygame.sprite.Group()
@@ -17,20 +18,22 @@ class General(pygame.sprite.Sprite): #clase general
         self.rect.centerx = x
         self.rect.centery = y
 
-    def draw (self, surface):  # "dibuja" a los objetos en pantalla
+    def draw(self, surface):  # "dibuja" a los objetos en pantalla
         surface.blit(self.image, (self.rect.x - self.rect.width / 2, self.rect.y - self.rect.height))
 
     def destroy (self, ClassName):  # destruye sprite
         ClassName.lista.remove (self)
         General.allsprites.remove(self)
         del self
+
+
 class Mundo(General):
     def __init__ (self, x, y, width, height, image_string, vel):
         General.__init__(self, x, y, width, height, image_string)
         self.vel = vel
 
     def move(self):
-        def move (self):
+        def move(self):
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 self.x += self.vel
@@ -40,6 +43,7 @@ class Mundo(General):
                 self.y += self.vel
             if keys[pygame.K_UP] or keys[pygame.K_w]:
                 self.y -= self.vel
+
 
 class Carros(General):
     lista = pygame.sprite.Group()
@@ -55,24 +59,50 @@ class Carros(General):
 
 class Jugador(Carros):
     lista = pygame.sprite.Group()
-    def __init__(self, x, y, width, height, image_string):
-        Carros.__init__(self, x, y, width, height, image_string)
+    def __init__(self, x, y, width, height, image_string, vel):
+        Carros.__init__(self, x, y, width, height, image_string, vel)
         Carros.lista.add(self)
 
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            pos_fondo[0] += self.vel
-            self.x -= self.vel
+            self.rect.x -= self.vel
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            pos_fondo[0] -= self.vel
-            self.x += self.vel
+            self.rect.x += self.vel
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            pos_fondo[1] -= self.vel
-            self.y += self.vel
+            self.rect.y += self.vel
         if keys[pygame.K_UP] or keys[pygame.K_w]:
-            pos_fondo[1] += self.vel
-            self.y -= self.vel
+            self.rect.y -= self.vel
 
 
-    
+class Enemigo(Carros):
+    lista = pygame.sprite.Group()
+
+    def __init__(self, x, y, width, height, image_string, vel):
+        Carros.__init__(self, x, y, width, height, image_string, vel)
+        Carros.lista.add(self)
+        self.velx = 0
+        self.vely = 0
+
+    def rand_spawn(self, fondo):
+        if random.randrange(100) > 95:
+            self.draw(fondo)
+            self.componentes_velocidad()
+
+    def componentes_velocidad(self): #toma la velocidad definida y la dirige a una direccion aleatoria, descomponiendo la velocidad en componentes
+        radianes = random.randrange(0, 2*math.pi, 0.02) #genera un numero aleatorio de radianes entre 0 y 2*pi, con un intervalo de 0. 2(approx. un grado)
+        x = round(self.vel*math.cos(radianes), 3)
+        y = round(self.vel*math.sin(radianes), 3)
+        self.velx = x
+        self.vely = y
+
+    def move(self, s_width, s_height): #mueve el vehiculo enemigo
+        if self.rect.x == s_width or self.rect.x == 0:
+            self.velx = -self.velx
+        if self.rect.y == s_height or self.rect.y ==0:
+            self.vely = -self.vely
+        self.rect.x += self.velx
+        self.rect.y += self.vely
+
+
+
---
+++ Game.py
@@ -0,0 +1,8 @@
+from Clases import *
+from pygame import *
+
+pygame.init()
+screen_size = [1280, 720]
+
+win = pygame.display.set_mode((screen_size[0], screen_size[1]))
+
